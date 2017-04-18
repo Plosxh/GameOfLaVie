51faa1e21b094cc22b350c1a761dfab5964229e6
CHARRAT pierre <pierre.charrat@teamwork.net>
2017-04-18 14:06:08 +0200

    add some classes

---
+++ Grille.py
@@ -0,0 +1,63 @@
+import random
+CONST_TERRE = 1
+CONST_EAU = 4
+CONST_MONTAGNE = 7
+CONST_MORT = 1
+CONST_VIVANT = 2
+CONST_ZOMBIE = 3
+
+class Grille:
+
+    def __init__(self,x,y):
+        self._grille = {}
+        self._largeur = y
+        self._longueur = x
+        for i in range(x):
+            rows = {}
+            for j in range(y):
+                random.seed(0)
+                rows[j]=random.choice([CONST_TERRE,CONST_EAU,CONST_MONTAGNE])
+            self._grille[i]=rows
+
+    def check_voisins(self,x,y):
+        nb_Mort = 0
+        nb_Zombie = 0
+        nb_Vivant = 0
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if i != 0 and j !=0:
+                    print("i")
+                    print(i)
+                    print("j")
+                    print(j)
+                    print("status")
+                    print(self._grille[x+i][y+j])
+                    if self._grille[x+i][y+j] in {2,5,8}:
+                        nb_Vivant+=1
+                    elif self._grille[x+i][y+j] in {3,6,9}:
+                        nb_Zombie+=1
+                    elif self._grille[x+i][y+j] in {1,4,7}:
+                        nb_Mort+=1
+        return nb_Mort,nb_Zombie,nb_Vivant
+
+
+    def get_Case(self,x,y):
+        try:
+            return self._grille[x][y]
+        except KeyError:
+            return "Error Out Of Bound"
+
+    def get_largeur(self):
+        return self._largeur
+
+    def get_longueur(self):
+        return self._longueur
+
+grille = Grille (10,10)
+mort,zombie,vivant = grille.check_voisins(5,5)
+print("mort")
+print(mort)
+print("vivant")
+print(vivant)
+print("zombie")
+print(zombie)
---
+++ Test.py
@@ -0,0 +1,26 @@
+import unittest
+from Grille import *
+
+class TestJeu(unittest.TestCase):
+
+    def modulo(self):
+        for i in range(9):
+            print(i)
+            print(i%3)
+
+        return True
+
+    def test_grilleGeneration(self):
+
+        #self.assertEqual(True,self.modulo())
+        grille = Grille(10,10)
+        self.assertEqual("Error Out Of Bound",grille.get_Case(40,1))
+        self.assertEqual(4,grille.get_Case(1,1))
+
+    def test_grilleInitialisation(self):
+        grille = Grille(10,10)
+
+        for i in range(grille.get_longueur()):
+            for j in range(grille.get_largeur()):
+                self.assertEqual(1,grille.get_Case(i,j)%3)
+unittest.main()
