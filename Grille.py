3ac90a22875361ce52ed644e3fe2e3f19d4f9ca9
CHARRAT pierre <pierre.charrat@teamwork.net>
2017-05-30 10:39:14 +0200

    improve test coverage

--- Grille.py
+++ Grille.py
@@ -23,28 +23,6 @@ class Grille:
             #print(rows)
             self._grille[i]=rows
 
-    def check_voisins(self,x,y):
-        nb_Mort = 0
-        nb_Zombie = 0
-        nb_Vivant = 0
-        for i in range(-1,2):
-            for j in range(-1,2):
-                if i != 0 and j !=0:
-                    print("i")
-                    print(i)
-                    print("j")
-                    print(j)
-                    print("status")
-                    print(self._grille[x+i][y+j])
-                    if self._grille[x+i][y+j] in {2,5,8}:
-                        nb_Vivant+=1
-                    elif self._grille[x+i][y+j] in {3,6,9}:
-                        nb_Zombie+=1
-                    elif self._grille[x+i][y+j] in {1,4,7}:
-                        nb_Mort+=1
-        return nb_Mort,nb_Zombie,nb_Vivant
-
-
     def get_Case(self,x,y):
         try:
             return self._grille[x][y]
